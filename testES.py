4885d4d8105a18cb0cfcbb4856002be3f88ec5ff
Haji Uzdenov <h.uzdenov@azintex.com>
2019-11-20 17:57:47 +0400

    mc

--- testES.py
+++ testES.py
@@ -1,15 +1,7 @@
 from elasticsearch import Elasticsearch
 import logging
 
-def connectES():
-    _es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-    if _es.ping():
-        print('Ping ok')
-    else:
-        print('Ping error')
-    return _es
-
-def createIndex(esObject, esIndexName='recipes'):
+def createIndex(esObject, indexName='recipes'):
     created = False
     settings = {
         "settings": {
@@ -26,7 +18,7 @@ def createIndex(esObject, esIndexName='recipes'):
     }
     try:
         if not esObject.indices.exists(esIndexName):
-            esObject.indices.create(index=esIndexName, ignore=400, body=settings)
+            esObject.indices.create(index=indexName, ignore=400, body=settings)
             print('Index created')
         created = True
     except Exception as ex:
@@ -35,7 +27,8 @@ def createIndex(esObject, esIndexName='recipes'):
         return created
 
 
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+createIndex(es)
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.ERROR)
-    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-    createIndex(es)
