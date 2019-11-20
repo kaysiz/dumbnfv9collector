695d1c42f86ef4bf8e3391b31d57cec9b9ee2eb6
unknown <h.uzdenov@azintex.com>
2019-11-21 02:01:33 +0400

    test

--- es.py
+++ es.py
@@ -16,7 +16,7 @@ def createIndex(esObject, indexName, indexBody):
     created = False
     try:
         if not esObject.indices.exists(indexName):
-            esObject.indices.create(index=indexName, ignore=400, body=indexBody)
+            esObject.indices.create(index=indexName, body=indexBody, ignore=400)
             print('Index created')
         created = True
     except Exception as ex:
